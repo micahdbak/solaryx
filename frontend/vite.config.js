ef1e0dbbfc4edaa5939b4c96ac6141771db1e701
Micah Baker <micahb923@gmail.com>
2026-02-28 14:20:01 -0800

    add tailwind

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,12 @@
+import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [tailwindcss(), sveltekit()],
 	server: {
 		proxy: {
-			'/api': {
-				target: 'http://localhost:3000',
-				changeOrigin: true
-			}
+			'/api': { target: 'http://localhost:3000', changeOrigin: true }
 		}
 	}
 });
